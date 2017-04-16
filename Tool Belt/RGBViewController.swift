21d5b24774ffeccdc3919e3660c0d354dcd842b4
jakeblack14 <jakeblack_14@yahoo.com>
2017-04-16 17:16:56 -0500

    Updated RGB

--- Tool Belt/RGBViewController.swift
+++ Tool Belt/RGBViewController.swift
@@ -7,15 +7,52 @@
 //
 
 import UIKit
+import Foundation
 
-class RGBViewController: UIViewController {
+
+
+
+class RGBViewController: UIViewController,
+    UIImagePickerControllerDelegate,
+UINavigationControllerDelegate  {
+    
     @IBOutlet weak var ErrorLabel: UILabel!
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    @IBAction func LongPressGesture(_ sender: UILongPressGestureRecognizer) {
+        
+        
+    }
+    let picker = UIImagePickerController()
 
+    @IBAction func photoFromLibrary(_ sender: UIButton) {
+        picker.allowsEditing = false
+        picker.sourceType = .photoLibrary
+        picker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+        present(picker, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        ErrorLabel.text = "This feature is unavailable on this device."
+        picker.delegate = self
+     
     }
+
+    func imagePickerController(_ picker: UIImagePickerController,didFinishPickingMediaWithInfo info: [String : AnyObject])
+    {
+        let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage //2
+        myImageView.contentMode = .scaleAspectFit //3
+        myImageView.image = chosenImage //4
+        dismiss(animated:true, completion: nil) //5
+    }
+    
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            dismiss(animated: true, completion: nil)
+        }
+    }
+    
 
 
 }
