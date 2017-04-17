f0471815575462eed661f913eefbaa9ab773395e
jakeblack14 <jakeblack_14@yahoo.com>
2017-04-17 17:40:57 -0500

    Tested and Changed Private Function

--- Tool Belt/RGBViewController.swift
+++ Tool Belt/RGBViewController.swift
@@ -91,7 +91,7 @@ extension UIImage {
       
        
         
-        private func imagePickerController(_ picker: UIImagePickerController,didFinishPickingMediaWithInfo info: [String : AnyObject])
+        func imagePickerController(_ picker: UIImagePickerController,didFinishPickingMediaWithInfo info: [String : AnyObject])
         {
             let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage //2
             myImageView.contentMode = .scaleAspectFit //3
